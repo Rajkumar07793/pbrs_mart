cd93a25f42bda4c5074564636f8775ec329f0b56
Prashant1125 <145115819+Prashant1125@users.noreply.github.com>
2025-05-26 18:04:11 +0530

    api calling

--- lib/controllers/auth_controllers/login_controller.dart
+++ lib/controllers/auth_controllers/login_controller.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pbrs_mart/core/utils/routes.dart';
+import 'package:pbrs_mart/network/api_service.dart';
 
 class LoginController extends GetxController {
   final formKey = GlobalKey<FormState>();
 
+  final ApiService apiService = ApiService();
+
+  var isLoading = false.obs;
   var agreeToTerms = false.obs;
 
   final mobileController = TextEditingController();
   final passwordController = TextEditingController();
-  final forgetPwdController = TextEditingController();
+  final newPwdController = TextEditingController();
   final otpController = TextEditingController();
   final confirmController = TextEditingController();
 
   @override
   void onClose() {
-    mobileController.dispose();
     passwordController.dispose();
-    forgetPwdController.dispose();
     otpController.dispose();
     confirmController.dispose();
+    newPwdController.dispose();
     super.onClose();
   }
 
@@ -34,11 +38,105 @@ class LoginController extends GetxController {
     return formKey.currentState?.validate() ?? false;
   }
 
-  void submitLogin() {
-    if (validate()) {
-      // Implement your login logic here
-      // For example navigate to home or bottom nav bar
-      Get.offAllNamed('/bottom_nav'); // or your route
+  void submitLogin() async {
+    if (!validate()) return;
+
+    isLoading.value = true;
+    final response = await apiService.loginUser(
+      mobile: mobileController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+    isLoading.value = false;
+
+    if (response != null && response.statusCode == 200) {
+      Get.snackbar(
+        'Success',
+        'Login successful',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.teal,
+        colorText: Colors.white,
+        borderRadius: 12,
+        margin: EdgeInsets.all(16),
+        icon: Icon(Icons.check_circle_outline, color: Colors.white),
+        duration: Duration(seconds: 3),
+        animationDuration: Duration(milliseconds: 300),
+        forwardAnimationCurve: Curves.easeOutBack,
+      );
+      Get.offAllNamed(AppRoutes.bottomNav);
+    } else {
+      Get.snackbar(
+        'Login Failed',
+        'Invalid mobile or password',
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.teal,
+        colorText: Colors.white,
+        borderRadius: 12,
+        margin: EdgeInsets.all(16),
+        icon: Icon(Icons.check_circle_outline, color: Colors.white),
+        duration: Duration(seconds: 3),
+        animationDuration: Duration(milliseconds: 300),
+        forwardAnimationCurve: Curves.easeOutBack,
+      );
+    }
+  }
+
+  void requestResetPasswordOtp() async {
+    final mobile = mobileController.text;
+    if (mobile.isEmpty || mobile.length < 10) {
+      Get.snackbar("Error", "Enter a valid mobile number");
+      return;
+    }
+
+    isLoading.value = true;
+    final response = await apiService.sendResetPasswordOtp(mobile: mobile);
+    isLoading.value = false;
+
+    if (response != null && response.statusCode == 200) {
+      Get.snackbar(
+        "Success",
+        "OTP sent to your mobile",
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.teal,
+        colorText: Colors.white,
+        borderRadius: 12,
+        margin: EdgeInsets.all(16),
+        icon: Icon(Icons.check_circle_outline, color: Colors.white),
+      );
+    } else {
+      Get.snackbar(
+        "Error",
+        "Failed to send OTP",
+        snackPosition: SnackPosition.TOP,
+        backgroundColor: Colors.teal,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+  void submitResetPassword() async {
+    final mobile = mobileController.text;
+    final otp = otpController.text;
+    final password = newPwdController.text;
+    final confirm = confirmController.text;
+
+    if (password != confirm) {
+      Get.snackbar('Error', 'Passwords do not match');
+      return;
+    }
+
+    final response = await apiService.resetPassword(
+      mobile: mobile,
+      password: password,
+      confirmPassword: confirm,
+      otp: otp,
+    );
+
+    if (response != null && response.statusCode == 200) {
+      Get.back();
+      Get.snackbar('Success', 'Password has been reset');
+    } else {
+      print(mobile);
+      Get.snackbar('Error', 'Failed to reset password');
     }
   }
 }
--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -2,15 +2,18 @@ import 'package:get/get.dart';
 import 'package:pbrs_mart/views/auth/login.dart';
 import 'package:pbrs_mart/views/auth/sign_up.dart';
 import 'package:pbrs_mart/views/dashboard/splash_screen.dart';
+import 'package:pbrs_mart/views/widgets/custom_bottom_bar.dart';
 
 class AppRoutes {
   static final routes = [
     GetPage(name: '/Splash', page: () => SplashScreen()),
     GetPage(name: '/Login', page: () => LoginScreen()),
     GetPage(name: '/SignUp', page: () => SignUpScreen()),
+    GetPage(name: '/BottomNav', page: () => CustomBottomNavBar()),
   ];
 
   static final String splash = '/Splash';
   static final String login = '/Login';
   static final String signUp = '/SignUp';
+  static final String bottomNav = '/BottomNav';
 }
